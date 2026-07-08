c314c860af5b80fea68163b073e95bea6412012e
Haroon Khan <haroonkhan6000@gmail.com>
2026-07-08 15:39:42 -0500

    Added functions in KeyValueStore.h

---
+++ include/KeyValueStore.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <unordered_map>
+#include <string>
+
+class KeyValueStore
+{
+private:
+    std::unordered_map<std::string, std::string> data;
+
+public:
+    void put(const std::string &key, const std::string &value);
+    bool get(const std::string &key, const std::string &value) const;
+    bool remove(const std::string &key);
+};
