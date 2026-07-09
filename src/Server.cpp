41e2f752b676a4fa7e37c591ea7f4cd1ec1ba0e7
Haroon Khan <haroonkhan6000@gmail.com>
2026-07-09 18:17:57 -0500

    Implemented start function using TCP sockets.

--- src/Server.cpp
+++ src/Server.cpp
@@ -15,7 +15,56 @@ Server::Server(int portNumber)
 
 void Server::start()
 {
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
+    if (serverSocket == -1)
+    {
+        std::cout << "ERROR: Failed to create socket." << std::endl;
+        return;
+    }
+
+    int option = 1;
+    setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(&option));
+
+    sockaddr_in serverAddress;
+    std::memset(&serverAddress, 0, sizeof(serverAddress));
+
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(port);
+
+    int bindResult = bind(serverSocket, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(serverAddress));
+
+    if (bindResult == -1)
+    {
+        std::cout << "ERROR: Failed to bind socket to port." << std::endl;
+        close(serverSocket);
+        return;
+    }
+
+    int listenResult = listen(serverSocket, 10);
+
+    std::cout << "KV server listening on port" << port << "..." << std::endl;
+
+    while (true)
+    {
+        sockaddr_in clientAddress;
+        socklen_t clientAddressSize = sizeof(clientAddress);
+
+        int clientSocket = accept(serverSocket, reinterpret_cast<sockaddr*>(&serverAddress), &clientAddressSize);
+
+        if (clientSocket == -1)
+        {
+            std::cout << "ERROR: Failed to accept client." << std::endl; 
+            continue;
+        }
+
+        handleClient(clientSocket);
+
+        close(clientSocket);
+    }
+
+    close(serverSocket);
 }
 
 void Server::handleClient(int clientSocket)
